bab5b43b97c6df8272b892b82f1285cba9632a3f
faruch ismailov <58651410+farucis@users.noreply.github.com>
2023-06-04 02:44:02 +0300

    responsibility to invitation

--- src/components/InvitationInfo/InvitationInfo.js
+++ src/components/InvitationInfo/InvitationInfo.js
@@ -3,15 +3,13 @@ import "./InvitationInfo.css";
 
 const InvitationInfo = () => {
   return (
-    
     <div id="InvitationInfo" className="center">
-        <h3 className="container-title">זמן</h3>
+      <h3 className="container-title">הזמנה</h3>
       <h2>אנו נרגשים להזמינכם לחגוג עמנו את יום נישואינו</h2>
       <h1>Faruch & Mapal</h1>
       <h2>שיערך ביום רביעי, כ"ז תשרי ה'תשפ"ד </h2>
       <h1>11.10.2023</h1>
       <div className="divider-img" />
-
     </div>
   );
 };

8c63373d86f197b6a91ad2fad91bf02c6369281b
faruch ismailov <58651410+farucis@users.noreply.github.com>
2023-06-05 01:26:04 +0300

    Start work to arrival confirmation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import InvitationInfo from "./components/InvitationInfo/InvitationInfo";
 import MapForPeople from "./components/MapForPeople/MapForPeople";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import AcceptEvent from "./components/AcceptEvent/AcceptEvent";
 
 function App() {
   React.useEffect(() => {
@@ -16,6 +17,7 @@ function App() {
       <AboutUs />
       <InvitationInfo />
       <MapForPeople />
+      <AcceptEvent />
       <BackToTopButton />
     </div>
   );
---
+++ src/components/AcceptEvent/AcceptEvent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./AcceptEvent.css";
+
+const AcceptEvent = () => {
+  return (
+    <div id="AcceptEvent" className="center">
+        <div className="title" >אישור הגעה</div>
+      <div class="buttons">
+        <button>כן</button>
+        <button>לא</button>
+      </div>
+    </div>
+  );
+};
+
+export default AcceptEvent;
